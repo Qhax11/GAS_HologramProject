479afd972c8e7dc005146aa25bd7f0df907e01a8
Şamil <45121107+Qhax11@users.noreply.github.com>
2023-05-01 18:45:05 +0300

    Hologram ability continued

---
+++ Source/GAS_Template/Private/AbilitySystem/TargetSystem/TargetActorHologram.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/TargetSystem/TargetActorHologram.h"
+#include "Abilities/GameplayAbility.h"
+#include "DrawDebugHelpers.h"
+
+ATargetActorHologram::ATargetActorHologram()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ATargetActorHologram::StartTargeting(UGameplayAbility* Ability)
+{
+	OwningAbility = Ability;
+	PrimaryPC = Cast<APlayerController>(Ability->GetAvatarActorFromActorInfo()->GetInstigatorController());
+	MasterPawn = PrimaryPC->GetPawn();
+	SetInstigator(InstigatorPawn);
+
+}
+
+// This function is called when Wait Target Data Task is confirmed
+void ATargetActorHologram::ConfirmTargetingAndContinue()
+{
+	InstigatorPawn->SetActorLocation(this->GetActorLocation());
+	FGameplayAbilityTargetDataHandle TargetData;
+	TargetDataReadyDelegate.Broadcast(TargetData);
+	UE_LOG(LogTemp, Warning, TEXT("BÝTTI"));
+
+}
+
+void ATargetActorHologram::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+
+	FCollisionQueryParams QueryParams;
+	QueryParams.bTraceComplex = false;
+	if (MasterPawn)
+	{
+		QueryParams.AddIgnoredActor(MasterPawn->GetUniqueID());
+	}
+
+	FHitResult HitResult;
+
+	FVector StartTrace = MasterPawn->GetActorLocation();
+	StartTrace.Z += HeightOfTrace;
+
+
+	FVector ViewPoint;
+	FRotator ViewRotation;
+	PrimaryPC->GetPlayerViewPoint(ViewPoint, ViewRotation);
+	FVector RotationVector = ViewRotation.Vector();
+
+	UE_LOG(LogTemp, Error, TEXT("%f, %f, %f"), RotationVector.X, RotationVector.Y, RotationVector.Z);
+	RotationVector = FVector(RotationVector.X * 500, RotationVector.Y * 500 , 0);
+	UE_LOG(LogTemp, Warning, TEXT("%f, %f, %f"), RotationVector.X, RotationVector.Y, RotationVector.Z);
+
+
+	bool TryTrace = GetWorld()->LineTraceSingleByChannel(HitResult, StartTrace, StartTrace + RotationVector, ECC_Visibility, QueryParams);
+	if (bDebug)
+		DrawDebugLine(GetWorld(), HitResult.TraceStart, HitResult.TraceEnd, FColor::Orange, false, 0.f, 0, 3.f);
+
+
+	FHitResult HitResult2;
+	bool TryTrace2 = GetWorld()->LineTraceSingleByChannel(HitResult2, HitResult.TraceEnd, FVector(HitResult.TraceEnd.X, HitResult.TraceEnd.Y,HitResult.TraceEnd.Z * -1), ECC_Visibility, QueryParams);
+	if (bDebug)
+		DrawDebugLine(GetWorld(), HitResult2.TraceStart, HitResult2.TraceEnd, FColor::Blue, false, 0.f, 0, 3.f);
+
+	SetActorLocation(HitResult2.Location);
+}
---
+++ Source/GAS_Template/Public/AbilitySystem/TargetSystem/TargetActorHologram.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbilityTargetActor.h"
+#include "TargetActorHologram.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_TEMPLATE_API ATargetActorHologram : public AGameplayAbilityTargetActor
+{
+	GENERATED_BODY()
+
+public:
+	ATargetActorHologram();
+
+	virtual void StartTargeting(UGameplayAbility* Ability) override;
+
+	virtual void ConfirmTargetingAndContinue() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ExposeOnSpawn), Category = "Hologram")
+		class APawn* InstigatorPawn;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ExposeOnSpawn), Category = "Hologram")
+		float HeightOfTrace;
+	UPROPERTY()
+		float LongOfTrace;
+
+
+	APawn* MasterPawn;
+	
+};
